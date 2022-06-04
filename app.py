70cf784fadc6a1f1b3cf24ac423eac234f374af0
Vruddhi <toliavruddhi@gmail.com>
2022-06-04 19:05:54 +0530

    tables created

--- app.py
+++ app.py
@@ -1,27 +1,41 @@
-from urllib import request
-from flask import Flask , render_template
-from models import *
+
+
+from flask import Flask , render_template,request
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://postgres:Vruddhi@9@localhost:5432/todo_list'
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://postgres:12345678@localhost:5432/todo_list'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
-db.init_app(app)
 
-@app.route('/')
-def home():
-    # if request.method == 'GET':
-        # todos=get_pending_todo()
-        db.create_all()
-        return render_template('index.html',todos=todos,cnt=len(todos))
+db=SQLAlchemy(app)
+db.init_app(app)
+class Todo(db.Model):
+    id =db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(20),nullable=False)
+    message=db.Column(db.String(200),nullable=False)
+    status=db.Column(db.String(10),nullable=False,default="pending")
     
-    # title=request.form.get('title')
-    # message=request.form.get('message')
-    # todo=request.form.get(title=title, message=message)
-    # db.session.add(todo)
-    # db.session.commit()
-    # return redirect('/')
+# with app.app_context():
+#     db.create_all()
+
+
+@app.route('/',methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        todos=get_pending_todo()
+        db.create_all()
+        return render_template('index.html')
+    
+    title=request.form.get('title')
+    message=request.form.get('message')
+    todo=request.form.get(title=title, message=message)
+    db.session.add(todo)
+    db.session.commit()
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    with app.app_context():
-        app.run(debug=True)
+    db.create_all()
+    app.run(debug=True)
     
--- models.py
+++ models.py
@@ -1,12 +1,14 @@
-from flask_sqlalchemy import SQLAlchemy
+from app import Todo
 
-db=SQLAlchemy()
 
-class Todo(db.Model):
-    id =db.Column(db.Integer, primary_key=True)
-    title=db.Column(db.String(20),nullable=False)
-    message=db.Column(db.String(200),nullable=False)
-    status=db.Column(db.String(10),nullable=False,default="pending")
+
+# db=SQLAlchemy(app)
+
+# class Todo(db.Model):
+#     id =db.Column(db.Integer, primary_key=True)
+#     title=db.Column(db.String(20),nullable=False)
+#     message=db.Column(db.String(200),nullable=False)
+#     status=db.Column(db.String(10),nullable=False,default="pending")
     
 def get_todo(todo_id):
     return Todo.query.filter(Todo.id==todo_id).first()
