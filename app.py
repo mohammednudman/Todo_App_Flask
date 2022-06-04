0c6528e24aa3fe7decebdf402ad6eb09b8b25139
Vruddhi <toliavruddhi@gmail.com>
2022-06-04 20:54:00 +0530

    Completed

--- app.py
+++ app.py
@@ -1,41 +1,49 @@
-
-
-from flask import Flask , render_template,request
-from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask , render_template,request,redirect
+from models import db,Todo
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://postgres:12345678@localhost:5432/todo_list'
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
 
-db=SQLAlchemy(app)
 db.init_app(app)
-class Todo(db.Model):
-    id =db.Column(db.Integer, primary_key=True)
-    title=db.Column(db.String(20),nullable=False)
-    message=db.Column(db.String(200),nullable=False)
-    status=db.Column(db.String(10),nullable=False,default="pending")
-    
+
 # with app.app_context():
 #     db.create_all()
-
 
 @app.route('/',methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
-        todos=get_pending_todo()
+        todos=Todo.get_pending_todo()
         db.create_all()
-        return render_template('index.html')
+        return render_template('index.html',todos=todos,cnt=len(todos))
     
     title=request.form.get('title')
     message=request.form.get('message')
-    todo=request.form.get(title=title, message=message)
+    todo=Todo(title=title, message=message)
     db.session.add(todo)
     db.session.commit()
-    return render_template('index.html')
+    return redirect('/')
+
+@app.route('/todos/complete/<int:todo_id>')
+def complete_todo(todo_id):
+    todo=Todo.get_todo(todo_id)
+    if todo is None:
+        return "<h1>Invalid todo id</h1>"
+    todo.status="completed"
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/todos/delete/<int:todo_id>')
+def delete_todo(todo_id):
+    todo = Todo.get_todo(todo_id)
+    if todo is None:
+        return "<h1>Invalid todo id</h1>"
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
 
 if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True)
+    with app.app_context():
+        app.run(debug=True)
     
--- models.py
+++ models.py
@@ -1,22 +1,19 @@
-from app import Todo
+from flask_sqlalchemy import SQLAlchemy
 
+db=SQLAlchemy()
 
-
-# db=SQLAlchemy(app)
-
-# class Todo(db.Model):
-#     id =db.Column(db.Integer, primary_key=True)
-#     title=db.Column(db.String(20),nullable=False)
-#     message=db.Column(db.String(200),nullable=False)
-#     status=db.Column(db.String(10),nullable=False,default="pending")
+class Todo(db.Model):
+    id =db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(20),nullable=False)
+    message=db.Column(db.String(200),nullable=False)
+    status=db.Column(db.String(10),nullable=False,default="pending")
     
-def get_todo(todo_id):
-    return Todo.query.filter(Todo.id==todo_id).first()
+    def get_todo(todo_id):
+        return Todo.query.filter(Todo.id==todo_id).first()
 
-def get_pending_todo():
-    return Todo.query.filter(Todo.status=='pending').all()
+    def get_pending_todo():
+        return Todo.query.filter(Todo.status=='pending').all()
 
-def get_completed_todo():
-    return Todo.query.filter(Todo.status=='completed').all()
-
+    def get_completed_todo():
+        return Todo.query.filter(Todo.status=='completed').all()
     
